57d769c432ac7b2e9dc664f346dcef7912d6bcb1
WSixx <lucas.2007s@gmail.com>
2021-03-13 13:31:26 -0300

    talk to me

--- email-bot.py
+++ email-bot.py
@@ -3,26 +3,47 @@ from dotenv import load_dotenv
 import os
 import speech_recognition as sr
 
-from gtts import gTTS
-from playsound import playsound
+import pyttsx3
 
 load_dotenv()
 my_email = os.getenv('MY_GMAIL')
 
 listener =sr.Recognizer()
+engine = pyttsx3.init()
 
-try:
-    with sr.Microphone() as source:
-        print('listening...')
-        listener.adjust_for_ambient_noise(source)
-        voice = listener.listen(source)
-        info = listener.recognize_google(voice, language='pt-BR')
-        print(info)
-except:
-    pass
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def getInfo():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            listener.adjust_for_ambient_noise(source)
+            voice = listener.listen(source)
+            info = listener.recognize_google(voice, language='pt-BR')
+            return info.lower()
+    except:
+        pass
 
 def send_email():
     server = smtplib.SMTP('smtp.gmail.com',587)
     server.starttls()
     server.login(os.getenv('MY_GMAIL'), os.getenv('MY_PASSWORD'))
-    server.sendmail(my_email, 'lucas.2007s@gmail.com','First Message Text')
+    server.sendmail(my_email, 'lucas.2007s@gmail.com',getInfo())
+
+email_list = {
+    'Meu':'lucas.2007s@gmail.com',
+    'lsp':'lsp.2007s@gmail.com',
+}
+
+def get_email_info():
+    talk('Enviar email para quem?')
+    name = getInfo()
+    talk('Qual é o assunto do email?')
+    subject = getInfo()
+    talk('Me diga qual o texto do seu email')
+    body = getInfo()
+
+
+get_email_info()
